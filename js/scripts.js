5aa9292b6b88fe2c9995b31d59c06b213fa26d01
Adam Craig <ajcraig@suffolk.edu>
2016-04-01 09:58:58 -0700

    add error checking for non-numbers, falsey inputs, and negative numbers, as well as some basic styling/centering

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,31 @@ $(document).ready(function() {
 
   var pongify = function (number) {
 
-    var workingNumber = number;
+    var workingNumber = parseInt(number);
 
     var pongResults = [];
 
+    if ( !workingNumber ) {
+      alert("Come on, you have to enter an ACTUAL number.");
+    } else if ( workingNumber < 0 ) {
+      alert("Seriously? It's gotta be a POSITIVE number.")
+    } else {
+
     for (index = 1; index <= workingNumber; index++) {
       if ( (index % 3 === 0) && (index % 5 === 0) ) {
-        pongResults.push("ping-pong!");
+        pongResults.push("Ping-Pong!");
       } else if ( index % 3 === 0) {
-        pongResults.push("ping");
+        pongResults.push("Ping");
       } else if ( index % 5 === 0) {
-        pongResults.push("pong");
+        pongResults.push("Pong");
       } else {
         pongResults.push(index);
       }
     }
-      pongResults = pongResults.join(" ** ");
+      pongResults = pongResults.join(" * ");
       return pongResults;
     }
+  }
 
   // User Interface Logic
 
