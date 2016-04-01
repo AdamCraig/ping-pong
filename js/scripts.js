aa133cea8c582ab56616963f025399339d1a96bb
Adam Craig <ajcraig@suffolk.edu>
2016-04-01 09:19:52 -0700

    add core javascript functionality, add displaying proper output and refreshing results area each time form is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,31 @@ $(document).ready(function() {
 
   var pongify = function (number) {
 
-    var workingNumber = number.split("");
+    var workingNumber = number;
 
     var pongResults = [];
 
-    
-
-    return pongResults;
-
-  }
+    for (index = 1; index <= workingNumber; index++) {
+      if ( (index % 3 === 0) && (index % 5 === 0) ) {
+        pongResults.push("ping-pong!");
+      } else if ( index % 3 === 0) {
+        pongResults.push("ping");
+      } else if ( index % 5 === 0) {
+        pongResults.push("pong");
+      } else {
+        pongResults.push(index);
+      }
+    }
+      pongResults = pongResults.join(" ** ");
+      return pongResults;
+    }
 
   // User Interface Logic
 
   $("form").submit(function(event) {
     event.preventDefault();
+
+    $("#result").empty();
 
     var numberToPongify = $("#userNumber").val();
     var final = pongify(numberToPongify);
